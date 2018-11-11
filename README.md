1d018b90eccda27c783b5976cdb313026956756c
Mindey <mindey@qq.com>
2018-11-11 23:01:32 +0000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drivers
+
+The drivers shall define clients for [metaform](https://pypi.org/project/metaform/).
---
+++ setup.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2018 WeFindX Foundation, CLG.
+# All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='drivers',
+    version='0.0.0',
+    description='Integration of controllers to drive tools.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/wefindx/drivers',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='MIT',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=[
+    ],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    zip_safe=False
+)
