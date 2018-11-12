6d6b00076e7f4bbf7b05c8834b141972da0718e3
Mindey <mindey@qq.com>
2018-11-12 01:30:03 +0000

    upd

--- drivers/halfbakery/__init__.py
+++ drivers/halfbakery/__init__.py
@@ -1,5 +1,7 @@
 def harvest():
-    test_items = [{'a': 1}, {'b': 2}, {'c': 3}]
+    test_items = [{'-': 'https://someplace.com/something1', 'a': 1},
+                  {'-': 'https://someplace.com/something2', 'b': 2},
+                  {'-': 'https://someplace.com/something3', 'c': 3}]
 
     for item in test_items:
         yield item
