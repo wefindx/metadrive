01e78ca4735d8389fa9fae3a948098ec55d99cb9
Mindey <mindey@qq.com>
2018-11-14 23:01:44 +0000

    prototype for halfbakery API

--- metadrive/halfbakery/api.py
+++ metadrive/halfbakery/api.py
@@ -1,8 +1,37 @@
 # this includes the methods available to items of the source
 
 class Topic:
-    pass
 
+    def __init__(self, session):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def vote_plus(self):
+        pass
+
+    def vote_neutral(self):
+        pass
+
+    def vote_minus(self):
+        pass
 
 class Comment:
-    pass
+    def __init__(self, session):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
