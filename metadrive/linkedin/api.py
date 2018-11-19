b8d9db73223c34c24d09372320deabaa17e3765f
Mindey <mindey@qq.com>
2018-11-19 10:58:14 +0000

    bump

--- metadrive/linkedin/api.py
+++ metadrive/linkedin/api.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.keys import Keys
 class Topic:
     def __init__(self, topic_url, session):
         self.driver = session
+        self.driver.get(topic_url)
 
     def comment(self, text):
-        self.driver.get(topic_url)
         self.driver.find_element_by_class_name('mentions-texteditor__content').click()
         self.driver.send_keys(text)
         self.driver.find_element_by_class_name('feed-shared-comment-box__submit-button').click()
