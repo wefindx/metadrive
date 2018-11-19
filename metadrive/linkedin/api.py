0008b9a36d7f274204f602c1ce810719bb5287f4
Mindey <mindey@qq.com>
2018-11-19 11:14:31 +0000

    bump

--- metadrive/linkedin/api.py
+++ metadrive/linkedin/api.py
@@ -6,8 +6,9 @@ class Topic:
         self.driver.get(topic_url)
 
     def comment(self, text):
+        self.driver.find_element_by_class_name('button comment').click()
         self.driver.find_element_by_class_name('mentions-texteditor__content').send_keys(text)
-        self.driver.find_element_by_class_name('feed-shared-comment-box__submit-button').click()
+        self.driver.find_element_by_class_name('feed-shared-comment-box__submit-button button-primary-small').click()
 
 class Contact:
     pass
