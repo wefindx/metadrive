190f5aad41c6246a8bb94dfc24c35d5fffb60c8e
Mindey <mindey@qq.com>
2018-12-10 18:02:29 +0000

    add treebase.org, ubio.org

---
+++ metadrive/treebase/__init__.py
@@ -0,0 +1,9 @@
+__site_url__ = 'https://treebase.org'
+__base_url__ = ''
+
+
+# documentation
+# https://treebase.org/treebase-web/urlAPI.html
+# https://github.com/TreeBASE/treebase/wiki/API
+
+# http://purl.org/phylo/treebase/phylows/taxon/find?query=dcterms.title==%22Homo%20sapiens%22&format=rss1&recordSchema=tree
---
+++ metadrive/ubio/__init__.py
@@ -0,0 +1,6 @@
+__site_url__ = 'http://www.ubio.org'
+__base_url__ = 'http://ubio.org/webservices/'
+
+
+# documentation
+# http://ubio.org/index.php?pagename=xml_services
