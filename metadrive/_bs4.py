88d2334fd08775194e453c2074f0053d051f5a59
Mindey <mindey@qq.com>
2018-12-12 02:19:28 +0000

    extend get_soup by adding selenium

--- metadrive/_bs4.py
+++ metadrive/_bs4.py
@@ -1,5 +1,6 @@
 import bs4
 from metadrive._requests import get_session
+from metadrive._selenium import get_driver
 
 def get_soup(url, session=None, use='requests'):
     if use == 'requests':
@@ -13,6 +14,11 @@ def get_soup(url, session=None, use='requests'):
             data = None
             raise Exception("Response was wrong, error ({}): {}".format(
                 response.status_code, data))
+    elif use == 'selenium':
+        if session is None:
+            session = get_driver(headless=True)
+        session.get(url)
+        data = session.page_source
 
-        soup = bs4.BeautifulSoup(data, 'html.parser')
-        return soup
+    soup = bs4.BeautifulSoup(data, 'html.parser')
+    return soup
