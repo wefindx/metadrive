b380471fe0717ab02d8d2eca204781dad42a8375
Mindey <mindey@qq.com>
2019-04-03 07:44:22 +0000

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='metadrive',
-    version='1.3.7',
+    version='1.3.8',
     description='Integration of controllers to drive tools.',
     long_description=long_description,
     long_description_content_type='text/markdown',
