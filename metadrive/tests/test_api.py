7af31cf7783ae714d152b23f4c4bdb7fabbef780
Mindey <mindey@qq.com>
2019-04-29 12:00:25 +0000

    add a few tests

--- metadrive/tests/test_api.py
+++ metadrive/tests/test_api.py
@@ -7,3 +7,10 @@ def test_root(client):
     response = client.get('http://0.0.0.0:8000')
     assert response.text.startswith('<!doctype ht')
 
+def test_drivers(client):
+    response = client.get('http://0.0.0.0:8000/drivers')
+    assert type(response.json()) == list
+
+def test_drives(client):
+    response = client.get('http://0.0.0.0:8000/drives')
+    assert type(response.json()) == list
