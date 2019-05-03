7290d9c1136e4e524dfbb1d1062565c6e40b1546
Mindey <mindey@qq.com>
2019-05-03 14:52:59 +0000

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='metadrive',
-    version='1.4.13',
+    version='1.4.14',
     description='Integration of controllers to drive tools.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
