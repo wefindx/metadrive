11b0b3fddcd96fe66bf65dbea1d9b15ecd430239
Mindey <mindey@qq.com>
2019-06-15 13:34:29 +0000

    update load function, to use metatype.Dict

--- metadrive/__init__.py
+++ metadrive/__init__.py
@@ -20,6 +20,24 @@ if not os.path.exists(config.DEFAULT_LOCATION):
 from metaform import get_schema
 from metadrive import drives
 
+
+def load(data, iterator=False):
+    '''
+    takes: saved data item
+    returns: item instnace of the class from driver
+    '''
+
+    from metatype import Dict
+
+    if isinstance(data, str):
+        if os.path.isfile(data):
+            return instantiate(next(Dict.readin(data)))
+
+        return [instantiate(item) for item in Dict.readin(data)]
+
+    elif isinstance(data, dict):
+        return instantiate(data)
+
 def search(source, features: dict):
     '''
     Object discovery by features in source.
@@ -47,42 +65,6 @@ def create(source, features: dict):
         object's address, and success status and/or errors.
     '''
     raise NotImplemented
-
-def load(data):
-    '''
-    takes: saved data item
-    returns: item instnace of the class from driver
-    '''
-    if isinstance(data, str):
-        data = yaml.load(open(data, 'r'))
-
-    _id = data.get('-')
-    _drive = data.get('@')
-
-    if _id is not None and _drive is not None:
-
-        # parsing '@' field:                                    #sample: PyPI::halfbakery_driver==0.0.1:default.api.Topic
-
-        # TBD: refactor by importing from metatype
-        packman = _drive.split('::', 1)[0]                 #sample: PyPI  (Conan, NPM, Paket, etc.)
-        drivespec = _drive.split('::', 1)[-1]              #sample: halfbakery_driver==0.0.1:default.api.Topic
-        driver_name_version = drivespec.split(':',1)[0]         #sample: halfbakery_driver==0.0.1
-        driver_name = driver_name_version.split('==',1)[0]      #sample: halfbakery_driver
-        driver_version = driver_name_version.rsplit('==',1)[-1] #sample: 0.0.1
-        profile_name_pkg_path = drivespec.rsplit(':',1)[-1]     #sample: default.api.Topic
-        profile_name = profile_name_pkg_path.split('.',1)[0]    #sample: default
-        pkg_path = profile_name_pkg_path.split('.',1)[-1]       #sample: api.Topic
-
-        # TBD: refactor by reusing metadrive/api.py# around 90 line
-        ndriver = driver_name.replace('-', '_')
-        module = __import__(ndriver)
-        api = __import__('{}.api'.format(ndriver), fromlist=[ndriver])
-        classname = _drive.rsplit('.',1)[-1]
-        Klass = getattr(api, classname)
-
-        item = Klass(data)
-        item.drive = drives.get('{}:{}'.format(driver_name, profile_name))
-        return item
 
 
 def read(term, limit=None):
@@ -146,3 +128,32 @@ def read(term, limit=None):
         return method(limit=limit)
     else:
         return method()
+
+def instantiate(data):
+    _id = data.get('-')
+    _drive = data.get('@')
+
+    if _id is not None and _drive is not None:
+
+        # parsing '@' field:                                    #sample: PyPI::halfbakery_driver==0.0.1:default.api.Topic
+
+        # TBD: refactor by importing from metatype
+        packman = _drive.split('::', 1)[0]                 #sample: PyPI  (Conan, NPM, Paket, etc.)
+        drivespec = _drive.split('::', 1)[-1]              #sample: halfbakery_driver==0.0.1:default.api.Topic
+        driver_name_version = drivespec.split(':',1)[0]         #sample: halfbakery_driver==0.0.1
+        driver_name = driver_name_version.split('==',1)[0]      #sample: halfbakery_driver
+        driver_version = driver_name_version.rsplit('==',1)[-1] #sample: 0.0.1
+        profile_name_pkg_path = drivespec.rsplit(':',1)[-1]     #sample: default.api.Topic
+        profile_name = profile_name_pkg_path.split('.',1)[0]    #sample: default
+        pkg_path = profile_name_pkg_path.split('.',1)[-1]       #sample: api.Topic
+
+        # TBD: refactor by reusing metadrive/api.py# around 90 line
+        ndriver = driver_name.replace('-', '_')
+        module = __import__(ndriver)
+        api = __import__('{}.api'.format(ndriver), fromlist=[ndriver])
+        classname = _drive.rsplit('.',1)[-1]
+        Klass = getattr(api, classname)
+
+        item = Klass(data)
+        item.drive = drives.get('{}:{}'.format(driver_name, profile_name))
+        return item
